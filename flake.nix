f8a3caa592f934b1fb5936eb77e0afc48ea7bb25
Scott Scoble <scott.scoble@onebrief.com>
2025-05-28 14:34:30 -0700

    move scripts to hack and create flake to be used elsewhere

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Scott's custom tools for OneBrief development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+        tools = pkgs.stdenv.mkDerivation {
+            name = "wscoble-tools";
+            src = ./hack;
+            installPhase = ''
+                mkdir -p $out
+                cp $src/* $out
+            '';
+        };
+      in {
+        defaultPackage = tools;
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ ]; };
+    });
+}
