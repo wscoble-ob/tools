b0a5ceb022657819b8f1cba18ef2931acf38e2d2
Scott Scoble <scott.scoble@onebrief.com>
2025-10-02 10:56:20 -0700

    add patch-review-helm tool

--- flake.nix
+++ flake.nix
@@ -7,18 +7,32 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let 
+      let
         pkgs = nixpkgs.legacyPackages.${system};
+        runtimeDeps = with pkgs; [
+          gh
+          kubernetes-helm
+          awscli2
+          kubectl
+        ];
         tools = pkgs.stdenv.mkDerivation {
-            name = "wscoble-tools";
-            src = ./hack;
-            installPhase = ''
-                mkdir -p $out/bin
-                cp $src/* $out/bin
-            '';
+          name = "wscoble-tools";
+          src = ./hack;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/* $out/bin
+            chmod +x $out/bin/*
+            if [ -f $out/bin/patch-review-helm ]; then
+              wrapProgram $out/bin/patch-review-helm \
+                --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
+            fi
+          '';
         };
       in {
         defaultPackage = tools;
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ ]; };
-    });
+        devShell = pkgs.mkShell {
+          buildInputs = runtimeDeps;
+        };
+      });
 }
